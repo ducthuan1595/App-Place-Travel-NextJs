5fa1c4c663e6e92edbe26d378df5bae05da7d210
thuantruong <hi.tim.gi.the@gmail.com>
2023-12-28 23:00:47 +0700

    install prisma

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image className="rounded-full" width={30} height={30} src={'/images/placeholder.jpg'} alt="avatar" />
+  )
+};
+
+export default Avatar;
