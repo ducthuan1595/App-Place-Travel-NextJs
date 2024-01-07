e109c52ef7afacf361d8a527ded6bd495e204cab
thuantruong <hi.tim.gi.the@gmail.com>
2024-01-07 15:21:08 +0700

    handle login with google and github

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,9 +2,14 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({src}) => {
   return (
-    <Image className="rounded-full" width={30} height={30} src={'/images/placeholder.jpg'} alt="avatar" />
+    <Image className="rounded-full" width={30} height={30} src={src || '/images/placeholder.jpg'} alt="avatar" />
   )
 };
 
